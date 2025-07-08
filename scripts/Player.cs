0f5b8858392896de6c8c58d3b046396fa94a22bc
tBaronDar <thedarelis@gmail.com>
2025-07-08 23:21:10 +0300

    added new sprite

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -7,10 +7,15 @@ public partial class Player : CharacterBody2D
 
     [Export] public PackedScene ShotgunConeScene;
     private Gun gun;
+    private AnimatedSprite2D bodySprite;
+    private AnimatedSprite2D legSprite;
 
     public override void _Ready()
     {
         gun = GetNode<Gun>("gun");
+        bodySprite = GetNode<AnimatedSprite2D>("BodySprite");
+        legSprite = GetNode<AnimatedSprite2D>("LegSprite");
+
     }
 
     public override void _PhysicsProcess(double delta)
@@ -21,7 +26,7 @@ public partial class Player : CharacterBody2D
 
     public override void _Process(double delta)
     {
-        HandleInput();
+        HandleShoot();
     }
 
     private void HandleMovement()
@@ -31,12 +36,23 @@ public partial class Player : CharacterBody2D
 
         if (direction != Vector2.Zero)
         {
-            velocity = direction * Speed;
+            velocity = direction.Normalized() * Speed;
+            if (!bodySprite.IsPlaying() || bodySprite.Animation != "walk")
+            {
+                bodySprite.Play("walk");
+                legSprite.Play("walk");
+            }
         }
         else
         {
             velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
             velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+
+            if (bodySprite.Animation != "idle")
+            {
+                bodySprite.Stop();
+                legSprite.Stop();
+            }
         }
 
         Velocity = velocity;
@@ -46,10 +62,10 @@ public partial class Player : CharacterBody2D
     private void RotateTowardsMouse()
     {
         Vector2 toMouse = GetGlobalMousePosition() - GlobalPosition;
-        Rotation = (float)(toMouse.Angle() + Math.PI / 2);
+        Rotation = (float)toMouse.Angle();
     }
 
-    private void HandleInput()
+    private void HandleShoot()
     {
         if (Input.IsActionJustPressed("fire"))
         {
