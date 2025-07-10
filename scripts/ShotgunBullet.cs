854c779cec7bcd65eaabb02e31bff51cc2afabe2
tBaronDar <thedarelis@gmail.com>
2025-07-10 20:19:11 +0300

    added max traveling distance to the bullet

--- scripts/ShotgunBullet.cs
+++ scripts/ShotgunBullet.cs
@@ -4,9 +4,24 @@ using System;
 public partial class ShotgunBullet : Area2D
 {
 	[Export] public float Speed = 600f;
-	public Vector2 Direction= Vector2.Zero;
+	[Export] public float MaxDistance = 1000f;
+	public Vector2 Direction = Vector2.Zero;
+	private Vector2 _startPosition;
+
+	public override void _Ready()
+	{
+		_startPosition = Position;
+	}
+
 	public override void _Process(double delta)
 	{
-		Position +=Direction*Speed*(float)delta;
+		Position += Direction * Speed * (float)delta;
+
+		float travelled = Position.DistanceTo(_startPosition);
+
+		if (travelled > MaxDistance)
+		{
+			QueueFree();
+		}
 	}
 }
